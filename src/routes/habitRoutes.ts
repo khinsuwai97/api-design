9c67c469efae7a5fba1d38b96260aef7428a3b22
Khin Su Wai <khinsu@xenoptics.com>
2025-10-28 11:47:09 +0700

    added middleware

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,4 +1,14 @@
 import { Router } from 'express'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+import { z } from 'zod'
+
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+
+const completeParmasSchema = z.object({
+  id: z.string().max(3),
+})
 
 const router = Router()
 
@@ -6,7 +16,7 @@ router.get('/', (req, res) => {
   res.json({ message: 'Get all habits' })
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateBody(createHabitSchema), (req, res) => {
   res.json({ message: 'Habit created' }).status(201)
 })
 
@@ -14,8 +24,13 @@ router.delete('/:id', (req, res) => {
   res.json({ message: 'Habit deleted' })
 })
 
-router.post('/:id/complete', (req, res) => {
-  res.json({ message: `Mark habit ${req.params.id} complete` })
-})
+router.post(
+  '/:id/complete',
+  validateParams(completeParmasSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: `Mark habit ${req.params.id} complete` })
+  }
+)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { isTest } from '../env.ts'
 import authRoutes from './routes/authRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.use('/api/habits', habitRoutes)
 app.use('/api/users', userRoutes)
