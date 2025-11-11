df832d86eab908a9ee2f1b3f8eaa576d9e9545c6
Khin Su Wai <khinsu@xenoptics.com>
2025-11-11 15:08:06 +0700

    fixed in seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,7 @@
-import { users, habitTags, habits, entries, tags } from './schema.ts'
 import { db } from './connection.ts'
+import { users, habitTags, habits, entries, tags } from './schema.ts'
+
+import { fileURLToPath, pathToFileURL } from 'url'
 
 async function seed() {
   console.log('start seeding...')
@@ -16,7 +18,7 @@ async function seed() {
       .values({
         email: 'demo@habitracker.com',
         username: 'demouser',
-        password: 'passwrord',
+        password: 'password',
         firstName: 'Demo',
         lastName: 'User',
       })
@@ -57,7 +59,7 @@ async function seed() {
       await db.insert(entries).values({
         habitId: exericseHabit.id,
         completion_date: date,
-        note: i === 0 ? 'Great workdout today!' : null,
+        note: i === 1 ? 'Great workdout today!' : null,
       })
     }
     console.log('✅ Database seeded successfully!')
@@ -71,7 +73,7 @@ async function seed() {
   }
 }
 //  Run seed if this file is executed directly
-if (import.meta.url === `file://${process.argv[1]}`) {
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
   seed()
     .then(() => process.exit(0))
     .catch((error) => {
